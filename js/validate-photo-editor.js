a85d0f6d70550bb1ff4b17ebb4a6180d75cf78e9
Semon <byuvi@mail.ru>
2024-03-29 23:41:09 +0300

    добавляет валидацию формы и тэгов

--- js/validate-photo-editor.js
+++ js/validate-photo-editor.js
@@ -1,17 +1,79 @@
 const photoEditorForm = document.querySelector('.img-upload__form');
 const hashtagsInput = document.querySelector('.text__hashtags');
+const textAreaInput = document.querySelector('.text__description');
 const validHashtag = /^#[a-zа-яё0-9]{1,19}$/i;
-const pristine = new Pristine(photoEditorForm);
 
-// - Сравниваем текущий тег с валидным значением
-const validateHashTag = (currentHashTag) => validHashtag.test(currentHashTag);
+const getHashtagArray = () => hashtagsInput.value.split(' ');
+
+// - В какое место должна выводиться ошибка?
+const pristine = new Pristine(photoEditorForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: '.img-upload__field-wrapper--error'
+});
 
 
-// - Добавляем новое правило для валидации
-pristine.addValidator(hashtagsInput, validateHashTag)
+// - Правило: сравниваем введенные в строку теги с валидным значением
+const checksHashTagSpelling = () => {
+  // - создаем массив тегов введенных пользователем
+  const hashtagArray = getHashtagArray();
+
+  if (hashtagsInput.value === '') {
+    return true;
+  }
+
+  // - проверяем елемменты массива из введенных пользователем тегов
+  for (let i = 0; i < hashtagArray.length; i++) {
+    if (!validHashtag.test(hashtagArray[i])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// - Правило: не больше определенного числа hashtag
+const checkHastTagQuantity = () => {
+  const hashtagArray = getHashtagArray();
+
+  if (hashtagArray.length > 5) {
+    return false;
+  }
+
+  return true;
+};
+
+const checksDuplicatesHashTag = () => {
+  const hashtagArray = getHashtagArray().map((arrayElement) => arrayElement.toLowerCase());
+
+  const Duplicates = hashtagArray.filter((number, index, numbers) => numbers.indexOf(number) !== index);
+
+  if (Duplicates.length >= 1) {
+    return false;
+  }
+
+  return true;
+};
+
+const checkTextareaLength = () => {
+  if (textAreaInput.value.length === 140) {
+    return false;
+  }
+
+  return true;
+};
+
+// - Добавляем новые правила для валидации
+pristine.addValidator(hashtagsInput, checksHashTagSpelling, 'введён невалидный хэштег');
+pristine.addValidator(hashtagsInput, checkHastTagQuantity, 'превышено количество хэштегов');
+pristine.addValidator(hashtagsInput, checksDuplicatesHashTag, 'хэштеги не могут повторятся');
+pristine.addValidator(textAreaInput, checkTextareaLength, 'длина комментария не может быть больше 140 символов');
+
 
 photoEditorForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
+
+  //const hashtagArray = hashtagsInput.value.split(' ');
 
   const isValid = pristine.validate();
   if (isValid) {
@@ -20,4 +82,5 @@ photoEditorForm.addEventListener('submit', (evt) => {
     console.log('Форма невалидна');
   }
 });
+
 
